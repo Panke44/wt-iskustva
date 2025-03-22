f4f3dd7c72ff0f1125b04f58721048b841b5269e
Milos Panic <milos@Miloss-MacBook-Pro.local>
2025-03-22 22:51:35 +0100

    Added blog

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './CSS/main.scss';
 import Home from './Pages/Home';
 import Experiences from './Pages/Experiences';
 import Error404 from './Pages/Error404';
+import Blog from './Pages/Blog';
 
 import MilosPanic from './Pages/Experiences/block_milos_panic';
 import DanijelaBilandzic from './Pages/Experiences/block_danijela_bilandzic';
@@ -19,6 +20,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/iskustva" element={<Experiences/>} />
         <Route path="*" element={<Error404/>} />
+        
+        <Route path="/blog" element={<Blog/>} />
 
         <Route path="/blockisland/milos-panic" element={<MilosPanic/>} />
         <Route path="/blockisland/danijela-bilandzic" element={<DanijelaBilandzic/>} />
---
+++ src/Pages/Blog.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+const Blog = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className="blog">
+        <img src={require("../Images/Blog/blog.jpg")} alt="" />
+        <h1> Zašto Work and Travel? </h1>
+        <p>
+          Na korak ste od pravljenja najbolje odluke u svom životu, work and travel je mnogim ljudima, pa i nama, značajno promenio život i način razmišljanja. Velika je stvar uraditi ovako nešto, koliko god jednostavno izgledalo sa svim programima i pomoćnim sredstvima koja su danas dostupna. Razloga za prijavu je mnogo, a mi ćemo sada preći preko najznačajnijih.
+        </p>
+
+        <h2>Kulturno iskustvo</h2>
+        <p>Svakoga od nas je u jednom trenutku privlačila Amerika, jednostavno nemoguće je odoleti njenom ogromnom kulturnom uticaju. Odrastali smo uz njihove filmove, serije i muziku, prirodan sledeći korak je videti sva mesta o kojima ste toliko slušali.
+          Sve je tačno onako kako očekujete da bude i potpuno drugačije u isto vreme, biće kao da dopunjavate puzlu za koju imate samo okvir.
+        </p>
+
+        <p>Pored već poznatih scena, dočekaće vas i ljudi koji Ameriku nazivaju domom, koji takodje dolaze iz različitih zemalja širom sveta, svako od njih sa svojom kulturom i pričom. Amerikanci su većinski radoznao narod, suprotno popularnom mišljenju, i biće zainteresovani da podelite svoju priču i običaje sa njima. Pored amerikanaca na program dolaze i studenti širom sveta, često i iz nama drugačijih mesta, poput Karipskih ili Azijskih drzava. To su neretko ljudi sa kojima ćete najviše i raditi, pogotovo ako prvi put dolazite na program i stavljeni ste na mesta sa puno drugih studenata. Ovo pruža priliku da proširite svoje vidike i shvatite kako da funkcionišete u internacionalnom okruženju sa ljudima koji su potpuno drugačiji od vas.</p>
+
+        <p>Povrh svega imaćete priliku da iskusite stil života kakav do sada niste imali, živite u mestu koje ste oduvek gledali i testirate sebe na načine na koje nikada do sada niste, 8000 kilometara od kuće.</p>
+
+        <h2>Lični razvoj</h2>
+
+        <p>Na program je mnogima najbolje da idu sa društvom, tako jeste najzabavnije, ali ići sam je mozda korisnije za lični rast. Ako želite zaista da vidite za šta ste sposobni i kako se snalazite u svetu koji je odjednom mnogo veći, slobodno idite na program bez ikoga.
+
+          Meni je najveci doživljaj bio osećaj kada sam prve godine išao sam, nešto izmedju straha i uzbudjenja. Nije u pitanju neprijatan osećaj, čak štaviše je i osvezavajuć, vi i vaš ranac, sami na ogromnom kontinentu. Samoća je u meni izazvala potrebu da se bolje snadjem, kako na poslu tako i u društvenom okruženju, naterala me je da preduzimam korake kakve inače ne bih morao. Naučilo me je kako da prilazim ljudima i nalazim sa kime da provodim vreme, bilo to nekoliko minuta priče u metrou ili stvaranje prijateljstava. U velikim gradovima je ovaj aspekt još izazovniji zato što su zajednice studenata manje te ste primorani da se krećete van svog užeg okruženja.
+        </p>
+
+        <p>Nezavisnost ćete takodje ostvariti udaljavanjem od ljudi koji su ceo život brinuli o vama, kakav je slučaj za većinu nas. Nebitno je da li kod kuće živite sami ili sa nekim, ovaj program će vas staviti u okruženje koje je mnogo nestabilnije i udaljenije od zone komfora. Pričaćete jezikom koji vam nije maternji, radićete sa ljudima koji su potpuno drugačiji od vas i trudićete se više nego ikada pre kako biste našli svoje mesto ovde.</p>
+
+        <p>Samopouzdanje koje ostvarite na programu će vam kasnije značiti u svakom aspektu života, kako na ličnom nivou tako i na profesionalnom.
+        </p>
+
+        <h2>Avantura</h2>
+
+        <p>Ako ništa drugo, work and travel je avantura pre svega. Od trenutka kada sednete na avion počinje nešto što nikada pre niste doživeli, potpuni izlazak iz zone komfora. Šetati ulicama velikih gradova po prvi put je doživljaj kao nijedan drugi, umorni od leta želite sto više da vidite a u isto vreme da se snadjete kako biste našli put do svoje destinacije.</p>
+
+        <p>Često sam dolazak predstavlja sagu za sebe, od pronalaska prevoza od aerodroma do sledeće stanice, pa onda buseva, vozova i neretko i trajekta koji će vas odvesti tamo gde trebate da budete. Za neke destinacije će vam sigurno trebati 20+ sati puta samo kako biste stigli. A to je samo dolazak, posle toga vas čeka celo leto snalaženja u nepoznatom okruženju, balansiraćete kako da radite što više i doživite što više. Svaki dan ce biti izazov za sebe i zelećete da ga iskoristite na najbolji mogući način.</p>
+
+        <p>Pored posla tu su druženja, ekskurzije, žurke i sve drugo što možete da poželite ili vam je dostupno u okolini. Slobodne dane ćete ceniti više nego što mislite i činiće vam se da traju mnogo duže od 24 sata zbog količine stvari koje ćete probati da uradite sa vremenom koje imate. Ako ste na istočnoj obali probajte da nadjete načine kako da posetite neki od većih obližnjih gradova, poput New York-a, Filadelfije ili Bostona. Ili ako radite blizu nacionalnih parkova potrudite se da organizujete dnevne izlete po prirodi.</p>
+
+        <p>Gde god bili važno je da slobodno vreme koje imate iskoristite na najbolji mogući način, bilo to obilascima drugih mesta ili fokusiranjem na to gde ste, avantura može biti tamo gde je napravite.</p>
+
+        <h3>Putovanja</h3>
+
+        <p>Po meni najbitniji aspekt, ovaj program će vam omogućiti da istražite fantastično mesto koje su Sjedinjene države. Ako vam je oduvek bio san da posetite New York ili Los Angeles, sada to i možete.</p>
+
+        <p>Ne morate čak ni čekati kraj da biste imali priliku da putujete, urbaniji delovi države su dobro povezani, pogotovo na istoku, tako da ako ste blizu većih gradova iskoristite slobodne dane da ih posetite. Svašta se može uraditi za dan, od jednostavnog istraživanja, odmaranja na plaži, do poseta muzejima i pozorištima, sve zavisi od toga šta želite.</p>
+
+        <p>Kraj programa je period kada ćete stvarno imati vremena da vidite sve što ste zamislili, ako se potrudite moguće je za tri nedelje napraviti krug oko cele države, što sam ja uradio svoje druge godine. Amerika je odlično avionski povezana i u vrlo kratkom periodu se možete naći na drugoj obali, hiljadama kilometara od mesta gde ste radili.</p>
+
+        <p>Na ovom sajtu ćemo postavljati detaljne vodiče za sve bitne lokacije širom države, zasnovane kako na našim tako i na iskustvima drugih studenata. Cilj nam je da vas što bolje spremimo i informišemo za ovaj deo programa zato što je po nama putovanje najbitnije i nešto što će zauvek ostati sa vama, avantura života. Trajalo ono tri nedelje ili 5 dana, potrudićemo se da vam ponudimo iskustvo koje nikada nećete zaboraviti.</p>
+
+        <h2>Novčani benefit</h2>
+
+        <p>Ovo je mnogima glavni razlog odlaska na program, što u neku ruku i ima smisla. Amerika je jedna od najbogatijih država na svetu i minimalna plata po satu koju ćete tamo najverovatnije zaradjivati se moze porediti sa našim dnevnim platama.</p>
+
+        <p>Program je velika investicija sam po sebi, kada se uzme u obračun njegova cena, zajedno sa vizom, avionskom kartom i novcem za prvih nedelju/dve u US, ispadne vas od 2500$ do 3500$. Razlika od 1000$ postoji zato što morate sami da procenite koliko novca vam je potrebno za potrošnju dok ne dobijete prvu platu, obavezno se konsultujte oko vremena isplate sa poslodavcem. Investiciju ćete skoro sigurno nadoknaditi, veličina profita zavisi od toga kako i gde živite za vreme programa
+          i koliko putujete nakon radnog dela.
+        </p>
+
+        <p>Moja preporuka je da ako je glavni razlog vašeg odlaska novac, idite na sezonske destinacije poput Block Island-a, Cape Cod-a i sličnih bogatijih mesta. Tamo su radne prilike najbolje i nemate toliko mesta da trošite zaradjeno, ljudi koje znam da su radili na tim lokacijama su se vraćali sa 10.000$ profita, nekada i više. Novac koji zaradite na programu posle možete uložiti u buduće prilike ili druge potrebe koje imate kod kuće. Ova uštedjevina će vas učiniti nezavisnijim u godinama dok i dalje studirate i nemate toliko radnih prilika kada se vratite.</p>
+
+
+        <p>Naravno bitno je shvatiti da iako ga ima više, novac u Americi ne dolazi tek tako, ljudi koji su se vraćali sa pomenutim sumama novca su često radili duple smene celog leta kako bi ga zaradili. Sve u suštini zavisi od toga koliko ste voljni da uložite svoje vreme i trud, više rada, više para.
+        </p>
+
+        <h2>Konačna reč</h2>
+
+        <p>Nadamo se da vam je ovaj članak pomogao da se odlučite da preduzmete ovaj korak i da ćete se na programu provesti i na kraju vratiti kao što smo se i mi, promenjeni zauvek.</p>
+
+        <p>Amerika, sa svim svojim obećanjima i izazovima, usponima i padovima, gradovima, kanjonima i okeanima, čeka na vas, ili bolje reći, vi čekate na nju.</p>
+
+        <p>Od ulica New York-a do Kalifornijskih zalazaka sunca, cela zemlja, uzduž i popreko vam je na dohvat ruke, samo trebate da je ispružite.
+        </p>
+
+      </div>
+    </>
+  )
+}
+
+export default Blog
